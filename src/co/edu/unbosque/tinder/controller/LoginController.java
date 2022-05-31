72805e36395e8db454b0e1775b5b5d32026b58ba
andres.martinez <andres.martinez@macroproyectos.com>
2022-05-31 18:02:29 -0500

    Inicio con logica de LoginController

--- src/co/edu/unbosque/tinder/controller/LoginController.java
+++ src/co/edu/unbosque/tinder/controller/LoginController.java
@@ -1,20 +1,40 @@
 package co.edu.unbosque.tinder.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import co.edu.unbosque.tinder.model.UserModel;
 import co.edu.unbosque.tinder.view.MainView;
 
 public class LoginController {
+	static ArrayList<UserModel> userDB = new ArrayList<>();
+
 	private MainView mainView;
 
 	/**
 	 * @param mainView
 	 */
-	public LoginController(MainView mainView) {
+	public LoginController(MainView mainView, UserModel userModel) {
 		this.mainView = mainView;
-	}
 
-	static ArrayList<UserModel> userDB = new ArrayList<>();
+		this.mainView.getLoginBtn().addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				try {
+					String username = mainView.getUserFld().getText();
+					String password = mainView.getPasswordFld().getText();
+
+				} catch (Exception e1) {
+					System.out.println("Error inesperado");
+					e1.printStackTrace();
+				}
+
+			}
+
+		});
+	}
 
 }
