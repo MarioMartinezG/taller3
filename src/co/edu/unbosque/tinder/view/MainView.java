8a6e1280c5a55751af0a504ce5973972451fe908
Aulas Informáticas Universidad El Bosque <aulainformatica@unbosque.edu.co>
2022-05-31 17:55:33 -0500

    vista principal tinder

--- src/co/edu/unbosque/tinder/view/MainView.java
+++ src/co/edu/unbosque/tinder/view/MainView.java
@@ -1,5 +1,34 @@
 package co.edu.unbosque.tinder.view;
 
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
 public class MainView {
+	
+	private JFrame mainView;
+	private JComboBox<String> ingresarCbx;
+	private JButton ingresarBtn;
+	
+	public MainView() {
+
+		this.mainView = new JFrame();
+
+		JLabel titleLbl = new JLabel("Welcome to TINDER ");
+		titleLbl.setBounds(40, 20, 420, 30);
+		titleLbl.setFont(new Font("Serif", Font.BOLD, 20));
+		titleLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		this.mainView.add(titleLbl);
+		
+		this.mainView.setSize(500, 290); // JFrame dimensions
+		this.mainView.setLocationRelativeTo(null); // Centering JFrame vertically and horizontally
+		this.mainView.setTitle(" TINDER"); // Setting the title of the window
+		this.mainView.setLayout(null);
+		this.mainView.setVisible(true);
+	}
 
 }
