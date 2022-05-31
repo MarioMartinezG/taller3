8cd4c88cbb4fe62be08ef075e667dd251a84ed0c
Aulas Informáticas Universidad El Bosque <aulainformatica@unbosque.edu.co>
2022-05-31 17:34:40 -0500

    Inicio desarrollo MainView

--- src/co/edu/unbosque/tinder/view/MainView.java
+++ src/co/edu/unbosque/tinder/view/MainView.java
@@ -1,5 +1,22 @@
 package co.edu.unbosque.tinder.view;
 
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
 public class MainView {
 
+	private JFrame mainView;
+	
+	public MainView() {
+	this.mainView = new JFrame();
+	
+	JLabel titleLbl = new JLabel("Welcome to TINDER ");
+	titleLbl.setBounds(40, 20, 420, 30);
+	titleLbl.setFont(new Font("Serif", Font.BOLD, 20));
+	titleLbl.setHorizontalAlignment(SwingConstants.CENTER);
+	this.mainView.add(titleLbl);
+	}
 }
